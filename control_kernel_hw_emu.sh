084fe293a2263597ea3b76b5204eb976538b6c82
RhysHuo <RhysHuo_1120272267@163.com>
2022-07-27 15:44:32 +0100

    Update control_kernel_hw_emu.sh

--- control_kernel_hw_emu.sh
+++ control_kernel_hw_emu.sh
@@ -5,3 +5,4 @@ cp kernelMatrixmult.h ..
 cd ..
 rm -rf Fused_Multicores
 v++ -t hw_emu --platform xilinx_u250_gen3x16_xdma_4_1_202210_1 -c -k mmult_top_sw -o'kernelmult.hw_emu.xo' kernelMatrixmult_sw.cpp kernelMatrixmult.h xcl2.hpp
+v++ -t hw_emu --platform xilinx_u250_gen3x16_xdma_4_1_202210_1 --link kernelmult.hw_emu.xo --config mmult_top_sw.cfg -o'kernelmult.hw_emu.xclbin'
