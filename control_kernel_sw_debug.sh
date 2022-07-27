4d06df54f320b5c630e97ca48c0ac9d246843e52
RhysHuo <RhysHuo_1120272267@163.com>
2022-07-27 14:44:26 +0100

    Update control_kernel_sw_debug.sh

--- control_kernel_sw_debug.sh
+++ control_kernel_sw_debug.sh
@@ -1,8 +1,8 @@
-git clone https://github.com/RhysHuo/Fused_Vitis_Updata01.git
-cd Fused_Vitis_Updata01
+git clone https://github.com/RhysHuo/Fused_Multicores.git
+cd Fused_Multicores
 cp kernelMatrixmult_sw.cpp ..
 cp kernelMatrixmult.h ..
 cd ..
-rm -rf Fused_Vitis_Updata01
-v++ -t sw_emu --platform xilinx_u250_gen3x16_xdma_4_1_202210_1 -c -k kernelmult1_sw -o'kernelmult1.sw_emu.xo' kernelMatrixmult_sw.cpp kernelMatrixmult.h xcl2.hpp
-v++ -t sw_emu --platform xilinx_u250_gen3x16_xdma_4_1_202210_1 --link kernelmult1.sw_emu.xo -o'kernelmult1.sw_emu.xclbin'
+rm -rf Fused_Multicores
+v++ -t sw_emu --platform xilinx_u250_gen3x16_xdma_4_1_202210_1 -c -k mmult_top_sw -o'kernelmult.sw_emu.xo' kernelMatrixmult_sw.cpp kernelMatrixmult.h xcl2.hpp
+v++ -t sw_emu --platform xilinx_u250_gen3x16_xdma_4_1_202210_1 --link kernelmult.sw_emu.xo -o'kernelmult.sw_emu.xclbin'
