7ce5a84d38fe0c4b6b2d892db70ffb9490b91eb1
RhysHuo <RhysHuo_1120272267@163.com>
2022-07-27 14:53:46 +0100

    Update control_kernel_sw_debug.sh

--- control_kernel_sw_debug.sh
+++ control_kernel_sw_debug.sh
@@ -5,4 +5,4 @@ cp kernelMatrixmult.h ..
 cd ..
 rm -rf Fused_Multicores
 v++ -t sw_emu --platform xilinx_u250_gen3x16_xdma_4_1_202210_1 -c -k mmult_top_sw -o'kernelmult.sw_emu.xo' kernelMatrixmult_sw.cpp kernelMatrixmult.h xcl2.hpp
-v++ -t sw_emu --platform xilinx_u250_gen3x16_xdma_4_1_202210_1 --link kernelmult.sw_emu.xo -o'kernelmult.sw_emu.xclbin'
+v++ -t sw_emu --platform xilinx_u250_gen3x16_xdma_4_1_202210_1 --link kernelmult.sw_emu.xo --config mmult_top_sw.cfg -o'kernelmult.sw_emu.xclbin'
